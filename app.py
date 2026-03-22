5f776be8d40f9c2caf7606d482053797affc106e
NishA <djm7749@psu.edu>
2026-03-22 17:37:01 -0400

    Init login/signup page

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import sqlite3 as sql
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Add a new patient
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run()
